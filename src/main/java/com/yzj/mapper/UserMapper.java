de0b2f1dd9edbf66f2f59fba84402e2b83f5b214
YaoZhejie <1694670626@qq.com>
2023-11-15 20:51:34 +0800

    第一次提交

---
+++ src/main/java/com/yzj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.yzj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yzj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
