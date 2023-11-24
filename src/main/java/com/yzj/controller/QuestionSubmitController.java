fe7c463758c558b00feb131e50596a361b3fe685
YaoZhejie <1694670626@qq.com>
2023-11-24 09:14:56 +0800

    11.24

--- src/main/java/com/yzj/controller/QuestionSubmitController.java
+++ src/main/java/com/yzj/controller/QuestionSubmitController.java
@@ -4,7 +4,7 @@ import com.yzj.common.BaseResponse;
 import com.yzj.common.ErrorCode;
 import com.yzj.common.ResultUtils;
 import com.yzj.exception.BusinessException;
-import com.yzj.model.dto.QuestionSubmit.questionSubmitAddRequest;
+import com.yzj.model.dto.questionsubmit.QuestionSubmitAddRequest;
 import com.yzj.model.entity.User;
 import com.yzj.service.QuestionSubmitService;
 import com.yzj.service.UserService;
@@ -32,22 +32,21 @@ public class QuestionSubmitController {
     private UserService userService;
 
     /**
-     * 点赞 / 取消点赞
-     *
+     * 提交题目
      * @param questionSubmitAddRequest
      * @param request
-     * @return resultNum 本次点赞变化数
+     * @return 结果
      */
     @PostMapping("/")
     public BaseResponse<Integer> doThumb(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
             HttpServletRequest request) {
-        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getPostId() <= 0) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         // 登录才能点赞
         final User loginUser = userService.getLoginUser(request);
-        long postId = questionSubmitAddRequest.getPostId();
-        int result = questionSubmitService.doquestionSubmit(postId, loginUser);
+        long questionId = questionSubmitAddRequest.getQuestionId();
+        int result = questionSubmitService.doQuestionSubmit(questionId, loginUser);
         return ResultUtils.success(result);
     }
 
--- src/main/java/com/yzj/service/QuestionSubmitService.java
+++ src/main/java/com/yzj/service/QuestionSubmitService.java
@@ -1,7 +1,14 @@
 package com.yzj.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yzj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.yzj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.yzj.model.entity.QuestionSubmit;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yzj.model.entity.User;
+import com.yzj.model.vo.QuestionSubmitVO;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
 * @author 姚浙杰
@@ -9,5 +16,43 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2023-11-16 13:18:16
 */
 public interface QuestionSubmitService extends IService<QuestionSubmit> {
+    /**
+     * 题目提交
+     *
+     * @param questionSubmitAddRequest 题目提交信息
+     * @param loginUser
+     * @return
+     */
+    long doQuestionSubmit(QuestionSubmitAddRequest questionSubmitAddRequest, User loginUser);
 
+    /**
+     * 获取查询条件
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    QueryWrapper<QuestionSubmit> getQueryWrapper(QuestionSubmitQueryRequest questionSubmitQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param questionSubmit
+     * @param loginUser
+     * @return
+     */
+    QuestionSubmitVO getQuestionSubmitVO(QuestionSubmit questionSubmit, User loginUser);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionSubmitPage
+     * @param loginUser
+     * @return
+     */
+    Page<QuestionSubmitVO> getQuestionSubmitVOPage(Page<QuestionSubmit> questionSubmitPage, User loginUser);
+
+    int doQuestionSubmit(long questionId, User loginUser);
+
+    @Transactional(rollbackFor = Exception.class)
+    int doQuestionSubmitInner(long userId, long questionId);
 }
